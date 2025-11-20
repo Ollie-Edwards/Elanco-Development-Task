cd531fed850534bb55161d1d578f80b7ab88cf21
Ollie-Edwards <ollie3edwards@gmail.com>
2025-11-20 18:22:08 +0000

    implemented /anylitics/num_sightings API endpoint to calculate and return the number of sightings depending the time interval (daily, weekly, monthly or yearly), and 2 optional parameters location and species

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import sqlite3
 from datetime import datetime
 
@@ -12,22 +12,19 @@ def create_connection():
 async def root():
     return {"message": "Hello World"}
 
-
 @app.get("/sightings")
 def getSightings(
    startDate: datetime | None = None,
    endDate: datetime | None = None,
    location: str | None = None,
    species: str | None = None,
-   latinName: str | None = None
 ):
 
   connection = create_connection()
   connection.row_factory = sqlite3.Row
   cursor = connection.cursor()
-  connection.row_factory = sqlite3.Row
 
-  baseQuery = "SELECT * FROM TickSightings"
+  query = "SELECT * FROM TickSightings"
 
   conditions = []
   filterParams = []
@@ -46,19 +43,65 @@ def getSightings(
 
   if species:
     conditions.append("species = (?)")
-    filterParams.append(species)
+    filterParams.append(species)  
 
-  if latinName:
-    conditions.append("latinName = (?)")
-    filterParams.append(latinName)    
+  if conditions:
+    query += " WHERE " + "AND ".join(conditions)
 
-  if conditions != []:
-    finalQuery = f"{baseQuery} WHERE { 'AND '.join(conditions) }"
-  else:
-    finalQuery = baseQuery
+  res = cursor.execute(query, filterParams).fetchall()
 
-  res = cursor.execute(finalQuery, filterParams).fetchall()
+  connection.close()
 
+  return [dict(row) for row in res]
+
+@app.get("/analytics/num_sightings/")
+def getSightingsByRegion(
+    interval: str,
+    location: str | None = None,
+    species: str | None = None,
+):
+  
+  if interval not in ["daily", "weekly", "monthly", "yearly"]:
+    raise HTTPException(status_code=422, detail="Invalid interval")  
+
+  match interval:
+    case "daily":
+      strf_format = "%Y-%m-%d"
+    case "weekly":
+      strf_format = "%Y-%W"
+    case "monthly":
+      strf_format = "%Y-%m"
+    case "yearly":
+      strf_format = "%Y"
+
+  query  = f"""
+    SELECT 
+      strftime(?, date) AS year_week,
+      COUNT(*) AS num_sightings
+    FROM TickSightings
+  """
+
+  conditions = []
+  filterParams = [strf_format]
+  
+  if location:
+    conditions.append("location = (?)")
+    filterParams.append(location)
+
+  if species:
+    conditions.append("species = (?)")
+    filterParams.append(species)  
+
+  connection = create_connection()
+  connection.row_factory = sqlite3.Row
+  cursor = connection.cursor()
+
+  if conditions:
+    query += " WHERE " + "AND ".join(conditions)
+
+  query += " GROUP BY year_week ORDER BY year_week;"
+
+  res = cursor.execute(query, filterParams).fetchall()
   connection.close()
 
   return [dict(row) for row in res]
